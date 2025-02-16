17108e8c58a5bb499687c50ff4a032a8198b3e4a
thegrtnx <thegrtnx@duck.com>
2025-02-17 00:15:58 +0100

    🚀 Deploy: next-15

---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import type { Metadata, Viewport } from "next";
+import { MaxWidthWrapper, cn, ThemeProvider, InternetStatus } from "@/lib";
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+import { Toaster } from "sonner";
+import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
+import { Footer, Header } from "@/components/reususables";
+
+const appName = "TeensYouths";
+const appColor = "#ffffff";
+
+export const metadata: Metadata = {
+	title: `${appName} `,
+	description: "Designed to help you connect with the right person in a halal way.",
+	manifest: "/manifest",
+};
+
+export const viewport: Viewport = {
+	themeColor: appColor,
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html
+			lang="en"
+			suppressHydrationWarning>
+			<body className={cn("m-auto min-h-screen bg-background bg-center bg-no-repeat scroll-smooth antialiased")}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="light"
+					enableSystem
+					disableTransitionOnChange>
+					<NextTopLoader
+						color="#000000"
+						showSpinner={false}
+						easing="ease"
+					/>
+					<Header />
+					<MaxWidthWrapper>{children}</MaxWidthWrapper>
+					<Footer />
+
+					<Toaster
+						position="top-right"
+						expand={false}
+					/>
+					<InternetStatus />
+					<GoogleAnalytics gaId="" />
+					<GoogleTagManager gtmId="" />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,87 @@
+import type { Config } from "tailwindcss";
+import { heroui } from "@heroui/react";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
+	darkMode: ["class"],
+	content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./view/**/*.{ts,tsx}", "./utils/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1.3rem",
+				sm: "1.3rem",
+				md: "1.3rem",
+				lg: "2rem",
+				xl: "2.5rem",
+			},
+		},
+		extend: {
+			screens: {
+				xs: { max: "380px" },
+				sm: "600px",
+			},
+			animation: {
+				"blur-in": "blur-in 1s ease-out",
+			},
+			keyframes: {
+				"blur-in": {
+					"0%": {
+						filter: "blur(10px)",
+					},
+					"100%": {
+						filter: "blur(0px)",
+					},
+				},
+			},
+			colors: {
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				chart: {
+					"1": "hsl(var(--chart-1))",
+					"2": "hsl(var(--chart-2))",
+					"3": "hsl(var(--chart-3))",
+					"4": "hsl(var(--chart-4))",
+					"5": "hsl(var(--chart-5))",
+				},
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+		},
+	},
+	plugins: [tailwindcssAnimate, heroui()],
+} satisfies Config;
